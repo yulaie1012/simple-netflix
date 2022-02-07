233c8b7d79e23bd3a16708b0a60122828e7cc6db
Jason Luo <yulaie1012@gmail.com>
2022-02-07 17:13:00 +0800

    Connecting to the database

--- includes/config.php
+++ includes/config.php
@@ -3,4 +3,11 @@ ob_start();
 session_start();
 
 date_default_timezone_set("Asia/Taipei");
+
+try {
+  $con = new PDO("mysql:dbname=netflix;host=localhost", "root", "");
+  $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+} catch (PDOException $e) {
+  exit("Connection failed: " . $e->getMessage());
+}
 ?>
