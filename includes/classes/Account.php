19a7fb0f94c52d8b38e4b76806a63d9023762c30
Jason Luo <yulaie1012@gmail.com>
2022-02-07 17:57:16 +0800

    Validating the first name

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -1,9 +1,16 @@
 <?php
 class Account {
   private $con;
+  private $errorArray = array();
 
   public function __construct($con) {
     $this->con = $con;
+  }
+
+  public function validateFirstName($fn) {
+    if (strlen($fn) < 2 || strlen($fn) > 25) {
+      array_push($this->errorArray, "Temporary string.");
+    }
   }
 }
 ?>
