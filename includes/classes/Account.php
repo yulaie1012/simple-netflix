67420f1b27400af9dff1ccc4bde0e950bc87ff81
Jason Luo <yulaie1012@gmail.com>
2022-02-08 11:08:06 +0800

    Inserting the user details to the table

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -13,6 +13,26 @@ class Account {
     $this->validateUsername($un);
     $this->validateEmails($em, $em2);
     $this->validatePasswords($pw, $pw2);
+
+    if (empty($this->errorArray)) {
+      return $this->insertUserDetails($fn, $ln, $un, $em, $pw);
+    }
+
+    return false;
+  }
+
+  private function insertUserDetails($fn, $ln, $un, $em, $pw) {
+    $pw = hash("sha512", $pw);
+
+    $query = $this->con->prepare("INSERT INTO users (firstName, lastName, username, email, password)
+                                  VALUES (:fn, :ln, :un, :em, :pw)");
+    $query->bindValue(":fn", $fn);
+    $query->bindValue(":ln", $ln);
+    $query->bindValue(":un", $un);
+    $query->bindValue(":em", $em);
+    $query->bindValue(":pw", $pw);
+
+    return $query->execute();
   }
 
   private function validateFirstName($fn) {
