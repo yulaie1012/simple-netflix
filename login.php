69e85e293edb4c0de22d36506152b9f770f700b5
Jason Luo <yulaie1012@gmail.com>
2022-02-08 14:27:02 +0800

    Remembering the last entered values

--- login.php
+++ login.php
@@ -17,6 +17,12 @@ if (isset($_POST["submitButton"])) {
     header("Location: index.php");
   }
 }
+
+function getInputValue($name) {
+  if (isset($_POST[$name])) {
+    echo $_POST[$name];
+  }
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -35,7 +41,7 @@ if (isset($_POST["submitButton"])) {
         </div>
         <form method="POST">
           <?php echo $account->getError(Constants::$loginFailed); ?>
-          <input type="text" name="username" placeholder="Username" required>
+          <input type="text" name="username" placeholder="Username" value="<?php getInputValue("username"); ?>" required>
           <input type="password" name="password" placeholder="Password" required>
 
           <input type="submit" name="submitButton" value="SUBMIT">
