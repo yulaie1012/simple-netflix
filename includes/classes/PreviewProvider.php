faf95d032c8bbdb68129dd7d39bf376d36c25845
Jason Luo <yulaie1012@gmail.com>
2022-02-14 15:15:47 +0800

    Creating the preview provider class

---
+++ includes/classes/PreviewProvider.php
@@ -0,0 +1,14 @@
+<?php
+class PreviewProvider {
+  private $con, $username;
+
+  public function __construct($con, $username) {
+    $this->con = $con;
+    $this->username = $username;
+  }
+
+  public function createTrailer() {
+    echo "create trailer";
+  }
+}
+?>
