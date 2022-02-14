2f1d6622badf2df7d180ff4405dbd83060d61c73
Jason Luo <yulaie1012@gmail.com>
2022-02-14 21:44:49 +0800

    Styling the category sections

--- includes/classes/CategoryContainer.php
+++ includes/classes/CategoryContainer.php
@@ -43,7 +43,14 @@ class CategoryContainer {
       $entitiesHtml .= $previewProvider->createEntityPreviewSquare($entity);
     }
 
-    return $entitiesHtml . "<br />";
+    return "<div class='category'>
+              <a href='category.php?id=$categoryId'>
+                <h3>$title</h3>
+              </a>
+              <div class='entities'>
+                $entitiesHtml
+              </div>
+            </div>";
   }
 }
 ?>
