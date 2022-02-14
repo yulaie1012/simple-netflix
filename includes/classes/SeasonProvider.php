f32ebb631db2a61154d312f3186fc8a0a1c32fd4
Jason Luo <yulaie1012@gmail.com>
2022-02-14 22:11:56 +0800

    Outputing the season numbers

--- includes/classes/SeasonProvider.php
+++ includes/classes/SeasonProvider.php
@@ -9,6 +9,22 @@ class SeasonProvider {
 
   public function create($entity) {
     $seasons = $entity->getSeasons();
+
+    if (sizeof($seasons) == 0) {
+      return;
+    }
+
+    $seasonsHtml = "";
+
+    foreach ($seasons as $season) {
+      $seasonNumber = $season->getSeasonNumber();
+
+      $seasonsHtml .= "<div class='season'>
+                         <h3>Season $seasonNumber</h3>
+                       </div>";
+    }
+
+    return $seasonsHtml;
   }
 }
 ?>
