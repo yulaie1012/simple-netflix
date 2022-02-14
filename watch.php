b19eb49e24a95979282930dca8021634c0d1be88
Jason Luo <yulaie1012@gmail.com>
2022-02-14 22:36:07 +0800

    Creating the back button

--- watch.php
+++ watch.php
@@ -9,6 +9,12 @@ $video = new Video($con, $_GET["id"]);;
 $video->increaseViews();
 ?>
 <div class="watch-container">
+  <div class="video-controls watch-nav">
+    <button>
+      <i class="fa-solid fa-arrow-left"></i>
+    </button>
+    <h1><?php echo $video->getTitle(); ?></h1>
+  </div>
   <video controls autoplay>
     <source src="<?php echo $video->getFilePath(); ?>" type="video/mp4" />
   </video>
