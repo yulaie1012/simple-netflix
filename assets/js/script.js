76d819520ebd2dc290538842d9b14613b799bdf1
Jason Luo <yulaie1012@gmail.com>
2022-02-14 15:39:27 +0800

    Changing the icon on mute/unmute

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
 function toggleVolume(button) {
   const muted = $(".preview-video").prop("muted");
   $(".preview-video").prop("muted", !muted);
+
+  $(button).find("i").toggleClass("fa-volume-xmark");
+  $(button).find("i").toggleClass("fa-volume-high");
 }
