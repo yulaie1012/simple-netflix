c4fda42efa16cc7c0c84207c9e91f3bee2598be5
Jason Luo <yulaie1012@gmail.com>
2022-02-14 15:43:56 +0800

    Creating the category container class

---
+++ includes/classes/CategoryContainer.php
@@ -0,0 +1,23 @@
+<?php
+class CategoryContainers {
+  private $con, $username;
+
+  public function __construct($con, $username) {
+    $this->con = $con;
+    $this->username = $username;
+  }
+
+  public function showAllCategories() {
+    $query = $this->con->prepare("SELECT * FROM categories");
+    $query->execute();
+
+    $html = "<div class='preview-categories'>";
+
+    while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
+      $html .= $row["name"];
+    }
+
+    return $html . "</div>";
+  }
+}
+?>
--- index.php
+++ index.php
@@ -3,4 +3,7 @@ require_once("includes/header.php");
 
 $previewProvider = new PreviewProvider($con, $userLoggedIn);
 echo $previewProvider->createTrailer(null);
+
+$categoryContainer = new CategoryContainer($con, $userLoggedIn);
+echo $categoryContainer->showAllCategories();
 ?>
