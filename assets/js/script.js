703e2789af6da7a8109966034d6536f13e6febdb
Jason Luo <yulaie1012@gmail.com>
2022-02-15 10:59:03 +0800

    Updating the video progress as the video plays

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,17 +40,27 @@ function updateProgressTimer(videoId, username) {
       window.clearInterval(timer);
       timer = window.setInterval(function () {
         // TODO: update progress
+        updateProgress(videoId, username, event.target.currentTime);
       }, 3000);
     })
     .on("ended", function () {
       // TODO: set finished
+      console.log(2);
       window.clearInterval(timer);
     });
 }
 
 function addProgress(videoId, username) {
   $.post("ajax/addProgress.php", { videoId, username }, function (data) {
-    if (data !== null && data != "") {
+    if (data !== null && data !== "") {
+      alert(data);
+    }
+  });
+}
+
+function updateProgress(videoId, username, progress) {
+  $.post("ajax/updateProgress.php", { videoId, username, progress }, function (data) {
+    if (data !== null && data !== "") {
       alert(data);
     }
   });
