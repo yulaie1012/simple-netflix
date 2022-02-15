68305387a9f25ad506bfe8ee5f043e5da86295a8
Jason Luo <yulaie1012@gmail.com>
2022-02-15 11:31:58 +0800

    Getting the 'up next' video if there's no episodes

--- includes/classes/VideoProvider.php
+++ includes/classes/VideoProvider.php
@@ -13,6 +13,18 @@ class VideoProvider {
     $query->bindValue(":season", $currentVideo->getSeason());
     $query->bindValue(":episode", $currentVideo->getEpisode());
     $query->execute();
+
+    if ($query->rowCount == 0) {
+      $query = $con->prepare("SELECT * FROM videos
+                              WHERE season <= 1 AND episode <= 1 AND id != :videoId
+                              ORDER BY views DESC
+                              LIMIT 1");
+      $query->bindValue(":videoId", $currentVideo->getId());
+      $query->execute();
+    }
+
+    $row = $query->fetch(PDO::FETCH_ASSOC);
+    return new Video($con, $row);
   }
 }
 ?>
