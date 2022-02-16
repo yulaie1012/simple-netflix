89a290b004b580b937d585aca349795854d67b81
Jason Luo <yulaie1012@gmail.com>
2022-02-16 13:49:44 +0800

    Outputting the search results

--- ajax/getSearchResults.php
+++ ajax/getSearchResults.php
@@ -3,6 +3,7 @@ require_once("../includes/config.php");
 require_once("../includes/classes/SearchResultsProvider.php");
 require_once("../includes/classes/Entity.php");
 require_once("../includes/classes/EntityProvider.php");
+require_once("../includes/classes/PreviewProvider.php");
 
 if (isset($_POST["keyword"]) && isset($_POST["username"])) {
   $searchResultsProvider = new SearchResultsProvider($con, $_POST["username"]);
--- includes/classes/SearchResultsProvider.php
+++ includes/classes/SearchResultsProvider.php
@@ -9,6 +9,30 @@ class SearchResultsProvider {
 
   public function getResults($keyword) {
     $entities = EntityProvider::getSearchEntities($this->con, $keyword);
+
+    $html = "<div class='preview-categories no-scroll'>";
+    $html .= $this->getResultsHtml($entities);
+    $html .= "</div>";
+
+    return $html;
+  }
+
+  private function getResultsHtml($entities) {
+    if (sizeof($entities) == 0) {
+      return;
+    }
+
+    $entitiesHtml = "";
+    $previewProvider = new PreviewProvider($this->con, $this->username);
+    foreach ($entities as $entity) {
+      $entitiesHtml .= $previewProvider->createEntityPreviewSquare($entity);
+    }
+
+    return "<div class='category'>
+              <div class='entities'>
+                $entitiesHtml
+              </div>
+            </div>";
   }
 }
 ?>
