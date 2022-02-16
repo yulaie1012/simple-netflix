45a1f52cc0e49eada4add4d01832bb8cf86111b0
Jason Luo <yulaie1012@gmail.com>
2022-02-16 13:14:39 +0800

    Creating the search page

---
+++ search.php
@@ -0,0 +1,7 @@
+<?php
+require_once("includes/header.php");
+?>
+<div class="text-box-container">
+  <input type="text" class="search-input" placeholder="Search for something" />
+</div>
+<div class="results"></div>
